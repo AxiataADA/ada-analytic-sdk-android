4fcc5ada29d59cc635017def490d3698da2c0e1b
Anurag-Mehrotra <44048179+Anurag-Mehrotra@users.noreply.github.com>
2020-05-07 22:28:45 +0800

    Update Constants.kt

--- app/src/main/java/com/example/adaanalyticdemo/Constants.kt
+++ app/src/main/java/com/example/adaanalyticdemo/Constants.kt
@@ -1,8 +1,8 @@
 package com.example.adaanalyticdemo
 
 object Constants {
-    const val APP_NAME: String = "MY-CovidAnalysis"
-    const val URL: String = "http://100.25.108.92"
-    const val APP_KEY: String = "my-covidanalysis"
-    const val APP_SECRET: String = "ab14885ce0acfb2ed1a1795c6f86d896"
+    const val APP_NAME: String = "<app name>"
+    const val URL: String = "http://<ip or domain>"
+    const val APP_KEY: String = "<app bundle name>"
+    const val APP_SECRET: String = "<app secret key>"
 }
